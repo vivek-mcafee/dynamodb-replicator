d072afa9a4f8a89213f3b10ebe1cd8ca3ec79510
Vivek Anand Verma <vivek.anand.verma@intel.com>
2017-02-02 16:03:24 +0530

    Removed streambot dependency.

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
 var AWS = require('aws-sdk');
 var Dyno = require('dyno');
 var queue = require('queue-async');
-var streambot = require('streambot');
 var crypto = require('crypto');
+var AgentKeepAlive = require('agentkeepalive');
+var fs = require('fs');
 
 module.exports.replicate = replicate;
-module.exports.streambotReplicate = streambot(replicate);
 module.exports.backup = incrementalBackup;
-module.exports.streambotBackup = streambot(incrementalBackup);
 module.exports.snapshot = require('./s3-snapshot');
+module.exports.lambdaenvReplicate = LambdaEnv(replicate);
+module.exports.lambdaenvBackup = LambdaEnv(incrementalBackup);
+module.exports.agent = new AgentKeepAlive.HttpsAgent({
+    keepAlive: true,
+    maxSockets: Math.ceil(require('os').cpus().length * 16),
+    keepAliveTimeout: 60000
+});
+
+
+function LambdaEnv(service) {
+    return function loadEnvironment(event, context) {
+        console.log('Start time: %s', (new Date()).toISOString());
+        console.log('Event md5: %s', crypto.createHash('md5').update(JSON.stringify(event)).digest('hex'));
+        // for more debugging
+        console.log('event = ', JSON.stringify(event, null, 2));
+        console.log('context = ', JSON.stringify(context, null, 2));
+        console.log('Table :', process.env.ReplicaTable);
+        console.log('Region :', process.env.region);
+        console.log('Replica Endpoint: %s', process.env.ReplicaEndpoint);
+        var callback = context.done.bind(context);
+        if (fs.existsSync('config.env')) {
+            require('dotenv').config({path: 'config.env'})
+            console.log('Loaded environment from config.env');
+        }
+        service.call(context, event, callback);
+    };
+}
 
 function replicate(event, callback) {
     var replicaConfig = {
@@ -19,38 +45,47 @@ function replicate(event, callback) {
         maxRetries: 1000,
         httpOptions: {
             timeout: 750,
-            agent: streambot.agent
+            agent: module.exports.agent
         }
     };
+    console.log('Table :' + process.env.ReplicaTable);
+    console.log('Region :' + replicaConfig.region);
+    console.log('Replica Endpoint : ' + process.env.ReplicaEndpoint);
+    console.log('Access Key Id : ' + replicaConfig.accessKeyId);
+    console.log('Secret Access Key: ' + replicaConfig.secretAccessKey);
     if (process.env.ReplicaEndpoint) replicaConfig.endpoint = process.env.ReplicaEndpoint;
     var replica = new Dyno(replicaConfig);
 
     var keyAttrs = Object.keys(event.Records[0].dynamodb.Keys);
 
-    var allRecords = event.Records.reduce(function(allRecords, change) {
+    var allRecords = event.Records.reduce(function (allRecords, change) {
         var id = JSON.stringify(change.dynamodb.Keys);
         allRecords[id] = allRecords[id] || [];
         allRecords[id].push(change);
+        console.log('Key Id : ' + id);
+        console.log('Record : ' + JSON.stringify(allRecords));
         return allRecords;
     }, {});
 
-    var params = { RequestItems: {} };
-    params.RequestItems[process.env.ReplicaTable] = Object.keys(allRecords).map(function(key) {
+    var params = {RequestItems: {}};
+    params.RequestItems[process.env.ReplicaTable] = Object.keys(allRecords).map(function (key) {
         var change = allRecords[key].pop();
         if (change.eventName === 'INSERT' || change.eventName === 'MODIFY') {
+            console.log("Event Name : " + change.eventName);
             return {
-                PutRequest: { Item: Dyno.deserialize(JSON.stringify(change.dynamodb.NewImage)) }
+                PutRequest: {Item: Dyno.deserialize(JSON.stringify(change.dynamodb.NewImage))}
             };
         } else if (change.eventName === 'REMOVE') {
+            console.log("Event Name : " + change.eventName);
             return {
-                DeleteRequest: { Key: Dyno.deserialize(JSON.stringify(change.dynamodb.Keys)) }
+                DeleteRequest: {Key: Dyno.deserialize(JSON.stringify(change.dynamodb.Keys))}
             }
         }
     });
 
     (function batchWrite(requestSet, attempts) {
-        requestSet.forEach(function(req) {
-            if (req) req.on('retry', function(res) {
+        requestSet.forEach(function (req) {
+            if (req) req.on('retry', function (res) {
                 if (!res.error || !res.httpResponse || !res.httpResponse.headers) return;
                 if (res.error.name === 'TimeoutError') res.error.retryable = true;
                 console.log(
@@ -58,9 +93,9 @@ function replicate(event, callback) {
                     res.error.message,
                     res.httpResponse.headers['x-amzn-requestid'],
                     res.httpResponse.headers['x-amz-crc32'],
-                    req.params.RequestItems[process.env.ReplicaTable].map(function(req) {
+                    req.params.RequestItems[process.env.ReplicaTable].map(function (req) {
                         if (req.DeleteRequest) return req.DeleteRequest.Key;
-                        if (req.PutRequest) return keyAttrs.reduce(function(key, k) {
+                        if (req.PutRequest) return keyAttrs.reduce(function (key, k) {
                             key[k] = req.PutRequest.Item[k];
                             return key;
                         }, {});
@@ -69,13 +104,17 @@ function replicate(event, callback) {
             });
         });
 
-        requestSet.sendAll(100, function(errs, responses, unprocessed) {
+        requestSet.sendAll(100, function (errs, responses, unprocessed) {
             attempts++;
 
             if (errs) {
                 var messages = errs
-                    .filter(function(err) { return !!err; })
-                    .map(function(err) { return err.message; })
+                    .filter(function (err) {
+                        return !!err;
+                    })
+                    .map(function (err) {
+                        return err.message;
+                    })
                     .join(' | ');
                 console.log('[error] %s', messages);
                 return callback(errs);
@@ -92,7 +131,10 @@ function replicate(event, callback) {
 }
 
 function incrementalBackup(event, callback) {
-    var allRecords = event.Records.reduce(function(allRecords, action) {
+    console.log('Table *:' + process.env.ReplicaTable);
+    console.log('Region *:' + process.env.region);
+    console.log('Replica Endpoint *: ' + process.env.ReplicaEndpoint);
+    var allRecords = event.Records.reduce(function (allRecords, action) {
         var id = JSON.stringify(action.dynamodb.Keys);
 
         allRecords[id] = allRecords[id] || [];
@@ -104,20 +146,20 @@ function incrementalBackup(event, callback) {
         maxRetries: 1000,
         httpOptions: {
             timeout: 1000,
-            agent: streambot.agent
+            agent: module.exports.agent
         }
     };
 
     if (process.env.BackupRegion) params.region = process.env.BackupRegion;
-
+    console.log('process.env.BackupRegion *: ' + process.env.BackupRegion);
     var s3 = new AWS.S3(params);
     var q = queue();
 
-    Object.keys(allRecords).forEach(function(key) {
+    Object.keys(allRecords).forEach(function (key) {
         q.defer(backupRecord, allRecords[key]);
     });
 
-    q.awaitAll(function(err) {
+    q.awaitAll(function (err) {
         if (err) throw err;
         callback();
     });
@@ -125,8 +167,8 @@ function incrementalBackup(event, callback) {
     function backupRecord(changes, callback) {
         var q = queue(1);
 
-        changes.forEach(function(change) {
-            q.defer(function(next) {
+        changes.forEach(function (change) {
+            q.defer(function (next) {
                 var id = crypto.createHash('md5')
                     .update(JSON.stringify(change.dynamodb.Keys))
                     .digest('hex');
@@ -140,8 +182,8 @@ function incrementalBackup(event, callback) {
 
                 var req = change.eventName === 'REMOVE' ? 'deleteObject' : 'putObject';
                 if (req === 'putObject') params.Body = JSON.stringify(change.dynamodb.NewImage);
-
-                s3[req](params, function(err) {
+                console.log('New Image : ' + JSON.stringify(change.dynamodb.NewImage))
+                s3[req](params, function (err) {
                     if (err) console.log(
                         '[error] %s | %s s3://%s/%s | %s',
                         JSON.stringify(change.dynamodb.Keys),
@@ -149,7 +191,7 @@ function incrementalBackup(event, callback) {
                         err.message
                     );
                     next(err);
-                }).on('retry', function(res) {
+                }).on('retry', function (res) {
                     if (!res.error || !res.httpResponse || !res.httpResponse.headers) return;
                     if (res.error.name === 'TimeoutError') res.error.retryable = true;
                     console.log(
